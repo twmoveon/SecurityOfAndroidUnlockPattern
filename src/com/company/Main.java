887086dafc8f49468db7281893faa71dcf5d9489
TW <lfwutong@gmail.com>
2018-05-05 20:39:05 -0400

    Change smartway

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,33 +4,34 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int n = 3;
         DFS dfs = new DFS();
         long startTime=System.currentTimeMillis();   //获取开始时间
-        System.out.print(dfs.numberOfPatterns(6,6));//测试的代码段
+        System.out.print(dfs.numberOfPatterns(n,n));//测试的代码段
         long endTime=System.currentTimeMillis(); //获取结束时间
         System.out.println("DFS程序运行时间： "+(endTime-startTime)+"ms");
 
         BackTracking backTracking = new BackTracking();
         long startTime2=System.currentTimeMillis();   //获取开始时间
-        System.out.print(backTracking.numberOfPatterns(6,6));// .numberOfPatterns(2,9);//测试的代码段
+        System.out.print(backTracking.numberOfPatterns(n,n));// .numberOfPatterns(2,9);//测试的代码段
         long endTime2=System.currentTimeMillis(); //获取结束时间
         System.out.println("BackTracking程序运行时间： "+(endTime2-startTime2)+"ms");
 
         SmartWay smartWay = new SmartWay();
         long startTime3=System.currentTimeMillis();   //获取开始时间
-        System.out.print(smartWay.numberOfPatterns(6,6));// .numberOfPatterns(2,9);//测试的代码段
+        System.out.print(smartWay.numberOfPatterns(n,n));// .numberOfPatterns(2,9);//测试的代码段
         long endTime3=System.currentTimeMillis(); //获取结束时间
         System.out.println("SmartWay程序运行时间： "+(endTime3-startTime3)+"ms");
 
         BruteForce bruteForce = new BruteForce();
         long startTime4=System.currentTimeMillis();   //获取开始时间
-        System.out.print(bruteForce.count(3));
+        System.out.print(bruteForce.count(n));
         long endTime4=System.currentTimeMillis(); //获取结束时间
         System.out.println("BruteForth程序运行时间： "+(endTime4-startTime4)+"ms");
 
         DP dp = new DP();
         long startTime5=System.currentTimeMillis();   //获取开始时间
-        System.out.print(dp.numberOfPatterns(3,3));
+        System.out.print(dp.numberOfPatterns(n,n));
         long endTime5=System.currentTimeMillis(); //获取结束时间
         System.out.println("DP程序运行时间： "+(endTime5-startTime5)+"ms");
 
--- src/com/company/SmartWay.java
+++ src/com/company/SmartWay.java
@@ -5,14 +5,19 @@ public class SmartWay {
         return count(m, n, 0, 1, 1, 1, 1);
     }
     public int count(int m, int n, int used, int i1, int j1, int i2, int j2) {
-        int number = 0;
-        if (m <= 0) {
-            number = 1;
+        int number=0;
+        if(m<=0)//m为0规定为一
+        {
+            number=1;
+        }
+        if(n==0)//结束条件
+        {
+            return 1;
         }
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 int I = i2 + i, J = j2 + j, used2 = used | (1 << (i * 3 + j));
-                if (used2 > used && ((I % 2) == 1 || (J % 2) == 1 || (used2 & (1 << (I / 2 * 3 + J / 2))) != 0))
+                if (used2 > used && ((I % 2)==1 || (J % 2)==1 || (used2 & (1 << (I / 2 * 3 + J / 2)))!=0 ))
                     number += count(m - 1, n - 1, used2, i2, j2, i, j);
             }
         }
